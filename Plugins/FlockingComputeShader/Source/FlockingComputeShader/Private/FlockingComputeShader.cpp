e93fa49a996bb6ffd46c0aeada6b91ac2a87b067
王时青 <sophomore@duck.com>
2025-01-07 14:17:30 +0800

    format code

--- Plugins/FlockingComputeShader/Source/FlockingComputeShader/Private/FlockingComputeShader.cpp
+++ Plugins/FlockingComputeShader/Source/FlockingComputeShader/Private/FlockingComputeShader.cpp
@@ -16,112 +16,121 @@
 IMPLEMENT_MODULE(FFlockingComputeShader, FlockingComputeShader)
 
 
-void FFlockingComputeShader::StartupModule()
-{
-	OnPostResolvedSceneColorHandle.Reset();
-	bCachedParametersValid = false;
+void FFlockingComputeShader::StartupModule() {
+    OnPostResolvedSceneColorHandle.Reset();
+    bCachedParametersValid = false;
 
-	FString PluginShaderDir = FPaths::Combine(IPluginManager::Get().FindPlugin(TEXT("FlockingComputeShader"))->GetBaseDir(), TEXT("Shaders"));
-	AddShaderSourceDirectoryMapping(TEXT("/FlockingComputeShaders"), PluginShaderDir);
+    FString PluginShaderDir = FPaths::Combine(
+        IPluginManager::Get().FindPlugin(TEXT("FlockingComputeShader"))->GetBaseDir(),
+        TEXT("Shaders"));
+    AddShaderSourceDirectoryMapping(TEXT("/FlockingComputeShaders"), PluginShaderDir);
 }
 
-void FFlockingComputeShader::ShutdownModule()
-{
-	EndRendering();
-	ResetAllShaderSourceDirectoryMappings();
+void FFlockingComputeShader::ShutdownModule() {
+    EndRendering();
+    ResetAllShaderSourceDirectoryMappings();
 }
 
-void FFlockingComputeShader::BeginRendering()
-{
-	if (OnPostResolvedSceneColorHandle.IsValid())
-	{
-		return;
-	}
+void FFlockingComputeShader::BeginRendering() {
+    if (OnPostResolvedSceneColorHandle.IsValid()) {
+        return;
+    }
 
-	bCachedParametersValid = false;
+    bCachedParametersValid = false;
 
-	const FName RendererModuleName("Renderer");
-	IRendererModule* RendererModule = FModuleManager::GetModulePtr<IRendererModule>(RendererModuleName);
-	if (RendererModule)
-	{
-		OnPostResolvedSceneColorHandle = RendererModule->GetResolvedSceneColorCallbacks().AddRaw(this, &FFlockingComputeShader::PostResolveSceneColor_RenderThread);
-	}
+    const FName RendererModuleName("Renderer");
+    IRendererModule *RendererModule = FModuleManager::GetModulePtr<IRendererModule>(
+        RendererModuleName);
+    if (RendererModule) {
+        OnPostResolvedSceneColorHandle = RendererModule->GetResolvedSceneColorCallbacks().AddRaw(
+            this, &FFlockingComputeShader::PostResolveSceneColor_RenderThread);
+    }
 }
 
-void FFlockingComputeShader::EndRendering()
-{
-	if (!OnPostResolvedSceneColorHandle.IsValid())
-	{
-		return;
-	}
+void FFlockingComputeShader::EndRendering() {
+    if (!OnPostResolvedSceneColorHandle.IsValid()) {
+        return;
+    }
 
-	const FName RendererModuleName("Renderer");
-	IRendererModule* RendererModule = FModuleManager::GetModulePtr<IRendererModule>(RendererModuleName);
-	if (RendererModule)
-	{
-		RendererModule->GetResolvedSceneColorCallbacks().Remove(OnPostResolvedSceneColorHandle);
-	}
+    const FName RendererModuleName("Renderer");
+    IRendererModule *RendererModule = FModuleManager::GetModulePtr<IRendererModule>(
+        RendererModuleName);
+    if (RendererModule) {
+        RendererModule->GetResolvedSceneColorCallbacks().Remove(OnPostResolvedSceneColorHandle);
+    }
 
-	OnPostResolvedSceneColorHandle.Reset();
+    OnPostResolvedSceneColorHandle.Reset();
 }
 
-void FFlockingComputeShader::UpdateParameters(FShaderUsageExampleParameters& DrawParameters)
-{
-	RenderEveryFrameLock.Lock();
-	CachedShaderUsageExampleParameters = DrawParameters;
-	bCachedParametersValid = true;
-	RenderEveryFrameLock.Unlock();
+void FFlockingComputeShader::UpdateParameters(FShaderUsageExampleParameters &DrawParameters) {
+    RenderEveryFrameLock.Lock();
+    CachedShaderUsageExampleParameters = DrawParameters;
+    bCachedParametersValid = true;
+    RenderEveryFrameLock.Unlock();
 }
 
-void FFlockingComputeShader::PostResolveSceneColor_RenderThread(FRHICommandListImmediate& RHICmdList, class FSceneRenderTargets& SceneContext)
-{
-	if (!bCachedParametersValid)
-	{
-		return;
-	}
+void FFlockingComputeShader::PostResolveSceneColor_RenderThread(
+    FRHICommandListImmediate &RHICmdList,
+    class FSceneRenderTargets &SceneContext) {
+    if (!bCachedParametersValid) {
+        return;
+    }
 
-	// Depending on your data, you might not have to lock here, just added this code to show how you can do it if you have to.
-	RenderEveryFrameLock.Lock();
-	FShaderUsageExampleParameters Copy = CachedShaderUsageExampleParameters;
-	RenderEveryFrameLock.Unlock();
+    // Depending on your data, you might not have to lock here, just added this code to show how you can do it if you have to.
+    RenderEveryFrameLock.Lock();
+    FShaderUsageExampleParameters Copy = CachedShaderUsageExampleParameters;
+    RenderEveryFrameLock.Unlock();
 
-	Draw_RenderThread(Copy);
+    Draw_RenderThread(Copy);
 }
 
-void FFlockingComputeShader::Draw_RenderThread(const FShaderUsageExampleParameters& DrawParameters)
-{
-	check(IsInRenderingThread());
-	if (!DrawParameters.VelocityRenderTarget)
-	{
-		return;
-	}
+void FFlockingComputeShader::Draw_RenderThread(
+    const FShaderUsageExampleParameters &DrawParameters) {
+    check(IsInRenderingThread());
+    if (!DrawParameters.VelocityRenderTarget) {
+        return;
+    }
 
-	if (!DrawParameters.PositionRenderTarget)
-	{
-		return;
-	}
+    if (!DrawParameters.PositionRenderTarget) {
+        return;
+    }
 
-	FRHICommandListImmediate& RHICmdList = GRHICommandList.GetImmediateCommandList();
+    FRHICommandListImmediate &RHICmdList = GRHICommandList.GetImmediateCommandList();
 
+    if (!VelocityOutput.IsValid()) {
+        FPooledRenderTargetDesc ComputeShaderOutputDesc(FPooledRenderTargetDesc::Create2DDesc(
+            DrawParameters.GetRenderTargetSize(), PF_FloatRGBA, FClearValueBinding::None,
+            TexCreate_None,
+            TexCreate_ShaderResource | TexCreate_UAV, false));
 
-	if (!VelocityOutput.IsValid())
-	{
-		FPooledRenderTargetDesc ComputeShaderOutputDesc(FPooledRenderTargetDesc::Create2DDesc(DrawParameters.GetRenderTargetSize(), PF_FloatRGBA, FClearValueBinding::None, TexCreate_None, TexCreate_ShaderResource | TexCreate_UAV, false));
+        ComputeShaderOutputDesc.DebugName = TEXT("ShaderPlugin_ComputeShaderOutput");
+        GRenderTargetPool.FindFreeElement(RHICmdList, ComputeShaderOutputDesc, VelocityOutput,
+            TEXT("ShaderPlugin_ComputeShaderOutput"));
+    }
 
-		ComputeShaderOutputDesc.DebugName = TEXT("ShaderPlugin_ComputeShaderOutput");
-		GRenderTargetPool.FindFreeElement(RHICmdList, ComputeShaderOutputDesc, VelocityOutput, TEXT("ShaderPlugin_ComputeShaderOutput"));
-	}
+    if (!PositionOutput.IsValid()) {
+        FPooledRenderTargetDesc ComputeShaderOutputDesc(FPooledRenderTargetDesc::Create2DDesc(
+            DrawParameters.GetRenderTargetSize(), PF_FloatRGBA, FClearValueBinding::None,
+            TexCreate_None,
+            TexCreate_ShaderResource | TexCreate_UAV, false));
 
-	if (!PositionOutput.IsValid())
-	{
-		FPooledRenderTargetDesc ComputeShaderOutputDesc(FPooledRenderTargetDesc::Create2DDesc(DrawParameters.GetRenderTargetSize(), PF_FloatRGBA, FClearValueBinding::None, TexCreate_None, TexCreate_ShaderResource | TexCreate_UAV, false));
+        ComputeShaderOutputDesc.DebugName = TEXT("ShaderPlugin_ComputeShaderOutput2");
+        GRenderTargetPool.FindFreeElement(RHICmdList, ComputeShaderOutputDesc, PositionOutput,
+            TEXT("ShaderPlugin_ComputeShaderOutput2"));
+    }
 
-		ComputeShaderOutputDesc.DebugName = TEXT("ShaderPlugin_ComputeShaderOutput2");
-		GRenderTargetPool.FindFreeElement(RHICmdList, ComputeShaderOutputDesc, PositionOutput, TEXT("ShaderPlugin_ComputeShaderOutput2"));
-	}
-
-	FComputeShaderExample::RunComputeShader_RenderThread(RHICmdList, DrawParameters, VelocityOutput->GetRenderTargetItem().UAV, DrawParameters.VelocityRenderTarget->GetRenderTargetResource()->TextureRHI, PositionOutput->GetRenderTargetItem().UAV, DrawParameters.PositionRenderTarget->GetRenderTargetResource()->TextureRHI);
-	//FPixelShaderExample::DrawToRenderTarget_RenderThread(RHICmdList, DrawParameters, VelocityOutput->GetRenderTargetItem().TargetableTexture, PositionOutput->GetRenderTargetItem().TargetableTexture);
-	RHICmdList.CopyTexture(VelocityOutput->GetRenderTargetItem().ShaderResourceTexture, DrawParameters.VelocityRenderTarget->GetRenderTargetResource()->TextureRHI, FRHICopyTextureInfo());
-	RHICmdList.CopyTexture(PositionOutput->GetRenderTargetItem().ShaderResourceTexture, DrawParameters.PositionRenderTarget->GetRenderTargetResource()->TextureRHI, FRHICopyTextureInfo());
+    FComputeShaderExample::RunComputeShader_RenderThread(RHICmdList, DrawParameters,
+        VelocityOutput->GetRenderTargetItem().UAV,
+        DrawParameters.VelocityRenderTarget->GetRenderTargetResource()
+                      ->TextureRHI,
+        PositionOutput->GetRenderTargetItem().UAV,
+        DrawParameters.PositionRenderTarget->GetRenderTargetResource()
+                      ->TextureRHI);
+    //FPixelShaderExample::DrawToRenderTarget_RenderThread(RHICmdList, DrawParameters, VelocityOutput->GetRenderTargetItem().TargetableTexture, PositionOutput->GetRenderTargetItem().TargetableTexture);
+    RHICmdList.CopyTexture(VelocityOutput->GetRenderTargetItem().ShaderResourceTexture,
+        DrawParameters.VelocityRenderTarget->GetRenderTargetResource()->TextureRHI,
+        FRHICopyTextureInfo());
+    RHICmdList.CopyTexture(PositionOutput->GetRenderTargetItem().ShaderResourceTexture,
+        DrawParameters.PositionRenderTarget->GetRenderTargetResource()->TextureRHI,
+        FRHICopyTextureInfo());
 }
