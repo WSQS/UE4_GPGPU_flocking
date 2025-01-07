a7bcfcef9acbe5d4ae915df17ff84efd47207b93
王时青 <sophomore@duck.com>
2025-01-07 16:52:57 +0800

    Format code

--- Plugins/FlockingComputeShader/Source/FlockingComputeShader/Private/PixelShaderExample.h
+++ Plugins/FlockingComputeShader/Source/FlockingComputeShader/Private/PixelShaderExample.h
@@ -4,8 +4,9 @@
 #include "FlockingComputeShader.h"
 
 
-class FPixelShaderExample
-{
+class FPixelShaderExample {
 public:
-	static void DrawToRenderTarget_RenderThread(FRHICommandListImmediate& RHICmdList, const FShaderUsageExampleParameters& DrawParameters, FTextureRHIRef ComputeShaderVelocityOutput, FTextureRHIRef ComputeShaderPositionOutput);
+    static void DrawToRenderTarget_RenderThread(FRHICommandListImmediate &RHICmdList,
+        const FShaderUsageExampleParameters &DrawParameters,
+        FTextureRHIRef ComputeShaderVelocityOutput, FTextureRHIRef ComputeShaderPositionOutput);
 };
